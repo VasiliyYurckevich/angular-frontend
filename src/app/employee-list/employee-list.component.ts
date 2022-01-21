9e79910bf7b585c9e6623d6ff2a980acee107190
VasiliyYurckevich <newskill228@gmail.com>
2022-01-21 11:32:36 +0300

    1

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+// import { EmployeeService } from '../employee.service'
 
 @Component({
   selector: 'app-employee-list',
@@ -13,6 +14,12 @@ export class EmployeeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.getEmployees();
   }
 
+  private getEmployees(){
+    
+      this.employees = [{"id":2,"firstName":"2342","lastName":"234","emailId":"2342"},{"id":29,"firstName":"Admin","lastName":"admin","emailId":"admin@gmail.cm"},{"id":1324,"firstName":"1234","lastName":"1234","emailId":"1234"}]
+   
+  }
 }
