a6d36165fe78cbcbb1935f49d485a1742f4c1165
VasiliyYurckevich <newskill228@gmail.com>
2022-01-21 11:51:13 +0300

    need  diseble websec

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
-// import { EmployeeService } from '../employee.service'
+ import { EmployeeService } from '../employee.service'
 
 @Component({
   selector: 'app-employee-list',
@@ -11,15 +12,30 @@ export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.getEmployees();
   }
 
   private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data =>{
+      this.employees = data;
+    })
     
-      this.employees = [{"id":2,"firstName":"2342","lastName":"234","emailId":"2342"},{"id":29,"firstName":"Admin","lastName":"admin","emailId":"admin@gmail.cm"},{"id":1324,"firstName":"1234","lastName":"1234","emailId":"1234"}]
-   
   }
+
+  updateEmployee(id: number){
+    this.router.navigate(['update-employee', id]);
+  }
+  
+  deleteEmployee(id: number){
+      this.employeeService.deleteEmployee(id).subscribe( data => {
+        console.log(data);
+        this.getEmployees();
+      })
+
+  }
+
 }
